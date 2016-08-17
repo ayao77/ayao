5480983a9cf667ba62ace292b8b657fa211d6bf1
ayao <528587267@qq.com>
2016-08-17 09:42:37 +0800

    add static attribute

--- HelloWord.java
+++ HelloWord.java
@@ -1,5 +1,13 @@
 public class Hello{
 	public static void main(String args[]){
 		System.out.println("My name is ayao");
+		System.out.println("Happy New Year");
+		print();
+	}
+
+	public static void print(){
+		System.out.println("*************************");
+		System.out.println("*	   Hello World		*");
+		System.out.println("*************************");
 	}
 }
