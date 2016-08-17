8054a395a369f1e1ec1824a0c6cd5cec54696c47
ayao <528587267@qq.com>
2016-08-17 10:06:22 +0800

    hello print

--- HelloWord.java
+++ HelloWord.java
@@ -1,14 +1,6 @@
 public class Hello{
 	public static void main(String args[]){
-		System.out.println("My name is ayao");
-		System.out.println("Happy New Year");
-		print();
-	}
-
-	public static void print(){
-		System.out.println("*************************");
-		System.out.println("#########################");
-		System.out.println("*************************");
-		System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%");
+		System.out.println("ttttttt");
+		System.out.println("nnnnnnn");
 	}
 }
