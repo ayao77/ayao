c9ea0c62424def6576e9256aa4dcdd40a13f9410
ayao <528587267@qq.com>
2016-08-17 09:49:53 +0800

    commit HelloWord.java

--- HelloWord.java
+++ HelloWord.java
@@ -7,11 +7,7 @@ public class Hello{
 
 	public static void print(){
 		System.out.println("*************************");
-<<<<<<< HEAD
 		System.out.println("*	   Hello nihhao		*");
-=======
-		System.out.println("*	   Hello World		*");
->>>>>>> brh
 		System.out.println("*************************");
 	}
 }
